d3fe20d3878744b23a08e8d68259b10fd2133a52
mradocy-sfg <mradocy@seafloorgames.com>
2023-02-04 13:14:25 -0500

    implemented dirt particle effect

--- Roots/Assets/Scripts/Levels/DirtTile.cs
+++ Roots/Assets/Scripts/Levels/DirtTile.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 namespace Levels {
     public class DirtTile : Tile {
 
+        #region Inspector Fields
+
+        [Header("Prefabs")]
+
+        [SerializeField]
+        private GameObject _particleEffectPrefab = null;
+
+        [Header("Children")]
+
+        [SerializeField]
+        private Transform _particleSpawnPos = null;
+
+        #endregion
+
         public override bool IsPassable => true;
 
         public override void Interact() {
             AudioManager.Instance.PlayDirtBreak();
+            Instantiate(_particleEffectPrefab, _particleSpawnPos.position, Quaternion.identity);
         }
 
     }
