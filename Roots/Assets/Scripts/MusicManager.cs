91eb37590928fca8b907132259400f513d262eb1
en-riley <69689574+en-riley@users.noreply.github.com>
2023-02-04 12:21:58 -0800

    Randomized Music Arrangement

--- Roots/Assets/Scripts/MusicManager.cs
+++ Roots/Assets/Scripts/MusicManager.cs
@@ -5,48 +5,22 @@ using UnityEngine;
 // [RequireComponent(typeof(AudioSource))]
 public class MusicManager : MonoBehaviour
 {
-    #region Inspector
-
-    [SerializeField]
-	public List<AudioClip> LevelMusic;
-	public List<AudioClip> MenuMusic;
-
-    #endregion
-
-    private static MusicManager _instance;
-
-    public static MusicManager Instance => _instance;
-
+    public AudioSource[] musicTracks;
+    private int currentTrack = 0;
     private AudioSource audioSource;
+	public int musicLayers;
 
-    void Awake()
+    private void Start()
     {
-        if (_instance != null)
-        {
-            Debug.LogError($"There can only be one {nameof(Game)}");
-            Destroy(this.gameObject);
-            return;
-        }
-        _instance = this;
-
-        audioSource = GetComponent<AudioSource>();
+        PlayMusicLayers();
     }
 
-    private void PlayMusic(AudioClip clip)
+    public void PlayMusicLayers()
     {
-        audioSource.PlayOneShot(clip, 1);
-    }
+        if (musicTracks.Length == 0) return;
 
-    // chooses a random clip from the list
-    private void PlayMusic(List<AudioClip> clips)
-    {
-        var idx = Random.Range(0, clips.Count);
-        PlayMusic(clips[idx]);
-    }
-
-    // example audio
-    public void PlayLevelMusic()
-    {
-        PlayMusic(LevelMusic);
+	for(int i = 0; i < musicLayers; i++){
+	musicTracks[Random.Range(0,musicTracks.Length)].mute = false;
+	}
     }
 }
