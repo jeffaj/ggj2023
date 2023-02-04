22e06c6a25fe6ae3cd5922774313079eb9bdb6f5
en-riley <69689574+en-riley@users.noreply.github.com>
2023-02-04 07:08:19 -0800

    Various tiles

---
+++ Roots/Assets/Scripts/Levels/FuelTile.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Levels {
+    public class FuelTile : Tile {
+    }
+}
---
+++ Roots/Assets/Scripts/Levels/RockTile.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Levels {
+    public class RockTile : Tile {
+    }
+}
