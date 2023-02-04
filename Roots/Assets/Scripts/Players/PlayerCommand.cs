c99c6464348081d59c9c1f7f6b8a9e1522ea592a
mradocy-sfg <mradocy@seafloorgames.com>
2023-02-03 22:20:54 -0500

    basic grid movement

---
+++ Roots/Assets/Scripts/Players/PlayerCommand.cs
@@ -0,0 +1,8 @@
+namespace Players {
+    public enum PlayerCommand {
+        None,
+        Left,
+        Right,
+        Down,
+    }
+}
--- Roots/Assets/Scripts/Players/PlayerInput.cs
+++ Roots/Assets/Scripts/Players/PlayerInput.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 namespace Players {
     public static class PlayerInput {
-        public static bool LeftHeld => Input.GetKey(KeyCode.LeftArrow);
-        public static bool RightHeld => Input.GetKey(KeyCode.RightArrow);
+        public static bool LeftPressed => Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKeyDown(KeyCode.RightArrow);
+        public static bool LeftHeld => Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow);
+        public static bool RightPressed => Input.GetKeyDown(KeyCode.RightArrow) && !Input.GetKeyDown(KeyCode.LeftArrow);
+        public static bool RightHeld => Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow);
+        public static bool DownPressed => Input.GetKeyDown(KeyCode.DownArrow) && !Input.GetKeyDown(KeyCode.UpArrow);
+
 
     }
 }
