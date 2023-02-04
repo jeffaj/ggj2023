bfb8401564700f7a551300f9eb69d6cf4d57581a
mradocy-sfg <mradocy@seafloorgames.com>
2023-02-04 18:51:38 -0500

    made MusicManager a singleton

--- Roots/Assets/Scripts/MusicManager.cs
+++ Roots/Assets/Scripts/MusicManager.cs
@@ -10,29 +10,47 @@ public class MusicManager : MonoBehaviour
     private AudioSource chosenOne;
 	public int musicLayers;
 
+    private static MusicManager _instance = null;
+
+    private void Awake() {
+        if (_instance != null) {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     private void Start()
     {
         PlayMusicLayers();
     }
 
-    public void PlayMusicLayers()
+    private void PlayMusicLayers()
     {
         if (musicTracks.Length == 0) return;
 
-	for(int i = 0; i < musicLayers; i++){
-	musicTracks[i].mute = true;
-	}
+	    for(int i = 0; i < musicLayers; i++){
+	    musicTracks[i].mute = true;
+	    }
 
-	for(int i = 0; i < musicLayers; i++){
-	chosenOne = musicTracks[Random.Range(0,musicTracks.Length)];
-	chosenOne.mute = false;
-	chosenOne.Play();
-	}
+	    for(int i = 0; i < musicLayers; i++){
+	    chosenOne = musicTracks[Random.Range(0,musicTracks.Length)];
+	    chosenOne.mute = false;
+	    chosenOne.Play();
+	    }
 	
     }
 
-	public void Update()
+	private void Update()
 	{
-		if(!chosenOne.isPlaying) PlayMusicLayers();		
+		if (!chosenOne.isPlaying)
+            PlayMusicLayers();		
 	}
+
+    private void OnDestroy() {
+        if (_instance == this) {
+            _instance = null;
+        }
+    }
 }
