c5eb4c6980bb7a84447527a6a1981e0494e0cf0a
Jeff Johnson <chubby.rhapsody@gmail.com>
2023-02-05 15:51:11 -0500

    grow shrink selectable

--- Roots/Assets/Scripts/UI/SpriteSwapSelectable.cs
+++ Roots/Assets/Scripts/UI/SpriteSwapSelectable.cs
@@ -1,22 +1,48 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class SpriteSwapSelectable : Selectable
 {
     public Sprite UnhighlightedSprite;
     public Sprite HighlightSprite;
 
+    private bool IsSelected = false;
+
     public override void OnHighlight()
     {
         gameObject.GetComponent<Image>().sprite = HighlightSprite;
+
+        IsSelected = true;
+
+        StartCoroutine(GrowShrink());
     }
 
     public override void OnDehighlight()
     {
         gameObject.GetComponent<Image>().sprite = UnhighlightedSprite;
+
+        IsSelected = false;
     }
 
     public override void OnSelectStyle()
     {
+    }
+
+    private IEnumerator GrowShrink()
+    {
+        float startTime = Time.unscaledTime;
+
+        while (IsSelected)
+        {
+            float elapsed = Time.unscaledTime - startTime;
+            gameObject.transform.localScale = Vector3.one + Vector3.one * 0.05f * Mathf.Sin(elapsed);
+            yield return null;
+        }
+
+        // now not selected
+
+        gameObject.transform.localScale = Vector3.one;
+
     }
 }
--- Roots/Assets/Scripts/UI/TextColorSelectable.cs
+++ Roots/Assets/Scripts/UI/TextColorSelectable.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-public class TextColorSelectable : Selectable {
-
+public class TextColorSelectable : Selectable
+{
     #region Inspector Fields
 
     [SerializeField]
@@ -17,13 +18,40 @@ public class TextColorSelectable : Selectable {
 
     #endregion
 
-    public override void OnHighlight() {
+    private bool IsSelected;
+
+    public override void OnHighlight()
+    {
         _text.color = _highlightColor;
+
+        IsSelected = true;
+
+        StartCoroutine(GrowShrink());
     }
 
-    public override void OnDehighlight() {
+    public override void OnDehighlight()
+    {
         _text.color = _dehighlightColor;
+
+        IsSelected = false;
     }
 
     public override void OnSelectStyle() { }
+
+    private IEnumerator GrowShrink()
+    {
+        float startTime = Time.unscaledTime;
+
+        while (IsSelected)
+        {
+            float elapsed = Time.unscaledTime - startTime;
+            gameObject.transform.localScale = Vector3.one + Vector3.one * 0.05f * Mathf.Sin(elapsed);
+            yield return null;
+        }
+
+        // now not selected
+
+        gameObject.transform.localScale = Vector3.one;
+
+    }
 }
